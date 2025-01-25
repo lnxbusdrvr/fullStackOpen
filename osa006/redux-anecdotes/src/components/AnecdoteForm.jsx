d0d6c36288ed7bee8b18c7f66c8ae1ac05a2c126
Erkki Pokkinen <erkki@vetokonsultit.fi>
2025-01-25 12:29:03 +0200

    6.15

--- osa006/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ osa006/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,18 +1,20 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notifyReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
 
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+    const newAnecdote = await anecdoteService.createNew(content)
 
-    dispatch(createAnecdote(content))
-    dispatch(setNotification(`you added ne note "${content}"`))
+    dispatch(createAnecdote(newAnecdote))
+    dispatch(setNotification(`you added ne note "${newAnecdote}"`))
   }
 
   return (
