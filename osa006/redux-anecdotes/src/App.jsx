71347550a798f6e46b058518f53bab27f96ea771
Erkki Pokkinen <erkki@vetokonsultit.fi>
2025-01-25 20:03:19 +0200

    6.16

--- osa006/redux-anecdotes/src/App.jsx
+++ osa006/redux-anecdotes/src/App.jsx
@@ -1,19 +1,20 @@
 import { useEffect } from 'react'
+
 import Header from './components/Header'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+
 import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 
 const App = () => {
   const dispatch = useDispatch()
-    useEffect(() => {
-      anecdoteService
-        .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
-    }, [])
+  useEffect(() => {
+    dispatch(initializeAnecdotes())
+  }, [])
 
   return (
     <div>
