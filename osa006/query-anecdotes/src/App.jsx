02a679838a3246485d47f33d2516024bddd81f27
Erkki Pokkinen <erkki@vetokonsultit.fi>
2025-01-28 21:05:28 +0200

    6.22

--- osa006/query-anecdotes/src/App.jsx
+++ osa006/query-anecdotes/src/App.jsx
@@ -1,13 +1,28 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, createAnecdote, updateVote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes']})
+    }
+  })
+
+  const updateVoteMutation = useMutation({
+    mutationFn: updateVote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes']})
+    }
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateVoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
   }
 
   const result = useQuery({
@@ -28,7 +43,7 @@ const App = () => {
       <h3>Anecdote app</h3>
     
       <Notification />
-      <AnecdoteForm />
+      <AnecdoteForm newAnecdoteMutation={newAnecdoteMutation} />
     
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
--- osa006/query-anecdotes/src/components/AnecdoteForm.jsx
+++ osa006/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,16 +1,4 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { createAnecdote  } from '../requests'
-
-const AnecdoteForm = () => {
-
-  const queryClient = useQueryClient()
-
-  const newAnecdoteMutation = useMutation({
-    mutationFn: createAnecdote,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['anecdotes']})
-    }
-  })
+const AnecdoteForm = ({ newAnecdoteMutation }) => {
 
   const onCreate = (event) => {
     event.preventDefault()
