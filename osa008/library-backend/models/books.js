2bf7b91ab1e1352096ae9ecac3d78e8b4a020ae6
Erkki Pokkinen <erkki@vetokonsultit.fi>
2025-02-15 20:06:24 +0200

    8.13

---
+++ osa008/library-backend/models/books.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+// you must install this library
+const uniqueValidator = require('mongoose-unique-validator')
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5
+  },
+  published: {
+    type: Number,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  },
+  genres: [
+    { type: String}
+  ]
+})
+
+schema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Book', schema)
+
+const mongoose = require('mongoose')
+
+const uniqueValidator = require('mongoose-unique-validator')
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 4
+  },
+  born: {
+    type: Number,
+  },
+})
+
+schema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Author', schema)
