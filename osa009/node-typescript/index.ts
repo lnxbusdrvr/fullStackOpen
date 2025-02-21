c2c71abe95e06b1769eecd50a1c69a59a159add9
Erkki Pokkinen <erkki@vetokonsultit.fi>
2025-02-21 20:11:47 +0200

    9.4

--- osa009/node-typescript/index.ts
+++ osa009/node-typescript/index.ts
@@ -1,5 +1,15 @@
-import calculateBmi from './bmiCalculator';
-import exerciseCalculator from './exerciseCalculator';
+//const express = require('express') as typeof import('express');
+//const express = require('express');
+import * as express from 'express';
+const app = express();
 
-console.log(calculateBmi(1.80, 74));
-console.log(exerciseCalculator(1.80, 74));
+app.get('/hello', (req: express.Request, res: express.Response) => {
+  res.send('\'Hello Full Stack!\'');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
