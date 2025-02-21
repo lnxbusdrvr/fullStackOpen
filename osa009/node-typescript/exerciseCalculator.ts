b3ebb80e1dddb10648770e70e5cbdafa80783afc
Erkki Pokkinen <erkki@vetokonsultit.fi>
2025-02-21 15:31:04 +0200

    9.3

--- osa009/node-typescript/exerciseCalculator.ts
+++ osa009/node-typescript/exerciseCalculator.ts
@@ -1,4 +1,7 @@
 
+import isNotNumber from './utils'
+
+
 interface ExercisesAvg {
   periodLength: number;
   trainingDays: number;
@@ -9,7 +12,7 @@ interface ExercisesAvg {
   average: number;
 }
 
-const calculateExercises = (dailyHours: number[], target: number): ExercisesAvg => {
+const calculateExercises = (target: number, dailyHours: number[]): ExercisesAvg => {
   const periodLength = dailyHours.length;
   const trainingDays = dailyHours.filter(hours => hours > 0).length;
   const totalHours = dailyHours.reduce((sum, hours) => sum + hours, 0);
@@ -22,7 +25,7 @@ const calculateExercises = (dailyHours: number[], target: number): ExercisesAvg 
   if (average >= target) {
     rating = 3;
     ratingDescription = 'Excellent! Party on Wayne';
-  } else if (average >= target * 0.75) {
+  } else if (average >= target * 0.6) {
     rating = 2;
     ratingDescription = 'Not too bad but could be better';
   } else {
@@ -41,6 +44,19 @@ const calculateExercises = (dailyHours: number[], target: number): ExercisesAvg 
   };
 };
 
-console.log(calculateExercises( [3, 0, 2, 4.5, 0, 3, 1], 2) )
+// Validate args
+const args: string[] = process.argv.slice(2); // removes node and filename
+if (args.length < 10) throw new Error('Not enough arguments');
+if (args.length > 10) throw new Error('Too many arguments');
+
+// Check all cliArg-types. some() gives all args by default
+if (args.some(isNotNumber)) {
+  throw new Error('All arguments should be numbers');
+}
+
+const dailyHours = args.slice(1, args.length).map(Number); //All except last one
+const target = Number(args[0]); 
+
+console.log(calculateExercises(target, dailyHours) )
 
 
