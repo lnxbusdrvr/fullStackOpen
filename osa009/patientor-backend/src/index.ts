d3d80f1ffd0823e1436f3310d9abb0bdb48b297d
Erkki Pokkinen <erkki@vetokonsultit.fi>
2025-02-25 23:53:55 +0200

    9.10

--- osa009/patientor-backend/src/index.ts
+++ osa009/patientor-backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 const cors = require('cors');//eslint-disable-line
 const app = express();
 import { Patient, getNonSensitiveEntries } from './data/patients';
+import { Diagnosis, getNonLatinDiagnosesEntries } from './data/diagnoses';
 
 app.use(express.json());
 app.use(cors());//eslint-disable-line
@@ -15,6 +16,11 @@ app.get('/api/ping', (_req: express.Request, res: express.Response) => {
 app.get('/api/patients', (_req: express.Request, res: express.Response<Patient[]>) => {
   res.json(getNonSensitiveEntries());
 });
+
+app.get('/api/diagnoses', (_req: express.Request, res: express.Response<Diagnosis[]>) => {
+  res.json(getNonLatinDiagnosesEntries());
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
