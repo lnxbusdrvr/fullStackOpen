001901887cface3cfef2fdbaf04487774c872f2e
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2025-04-27 15:15:11 +0300

    12.19

--- osa012/todo-app/todo-backend/dev.Dockerfile
+++ osa012/todo-app/todo-backend/dev.Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /usr/src/app
 
 COPY --chown=node:node . .
 
-RUN npm ci
+# Change npm ci to npm install since we are going to be in development mode
+RUN npm install
 
-CMD ["npx", "nodaemon", "index.js"]
+ENV DEBUG=todo-backend:*
+
+CMD ["npm", "run", "dev"]
--- osa012/todo-app/todo-frontend/vite.config.js
+++ osa012/todo-app/todo-frontend/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// server-stuff: to get rid off:
+// broswer-message: Blocked request. This host ("app") is not allowed.
+// To allow this host, add "app" to `server.allowedHosts` in vite.config.js.
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['app'],
+    host: true
+  }
 })
